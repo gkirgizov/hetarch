dd4b0e1b0a723aa7cc26ccfd910cb1e6d26178f8
gkirgizov <gkirgizov@gmail.com>
2018-01-09 22:46:33 +0300

    CodeLoader: basic sketch

--- new/CodeLoader.h
+++ new/CodeLoader.h
@@ -1,54 +1,101 @@
 #pragma once
 
 
-#include "IConnection.h
+#include "IConnection.h"
 #include "MemoryManager.h"
 #include "MemResident.h"
 
 #include "CodeGen.h"
-#include "IDSLCallable.h
+#include "IDSLCallable.h"
 #include "supportingClasses.h"
+
+#include "../tests/utils.h"
 
 
 namespace hetarch {
 
 template<typename AddrT, typename RetT, typename... Args>
-class ResidentObjCode : public MemResident, public IDSLCallable {
+class ResidentObjCode : public MemResident<AddrT>, public IDSLCallable<RetT, Args...> {
 
 public:
 
-    ResidentObjCode(mem::MemManager<AddrT> *memManager, mem::MemRegion<AddrT> memRegion, bool unloadable, AddrT addr)
-            : MemResident<AddrT>(memManager, memRegion, unloadable)
+    ResidentObjCode(mem::MemManager<AddrT> *memManager, mem::MemRegion<AddrT> memRegion,
+                    AddrT callAddr, bool unloadable)
+            : MemResident<AddrT>(memManager, memRegion, unloadable), callAddr(callAddr)
     {}
 
-    IDSLVariable<RetT> call(IDSLVariable<Args...>... args) override;
+    IDSLVariable<RetT> call(IDSLVariable<Args>... args) override {};
+
+    const AddrT callAddr;
 
 };
 
 
 // temporary here
 class CodeLoader {
+    using content_t = llvm::StringRef;
+
 public:
-
-    // todo: need IConnection there in interface
     template<typename AddrT, typename RetT, typename... Args>
-    static ResidentObjCode<RetT, Args...> load(mem::MemManager<AddrT> *memManager,
+    static ResidentObjCode<AddrT, RetT, Args...> load(conn::IConnection<AddrT> *conn,
+                                               mem::MemManager<AddrT> *memManager,
+                                               mem::MemType memType,
                                                const ObjCode<RetT, Args...> &objCode) {
-        // if not PIC then resolve symbols
-        // strip ObjCode from all extra and find its size
-        // alloc memory OF NEEDED TYPE
-        //      todo: how to determine what memType we need? always ask user?
-        // find displacement of Main Symbol (from the start of memRegion)
-        //      or maybe its final address
+        // if not PIC then relocate symbols ??? but it must be done after allocated memManager...
 
-        // create unloadable resident
-        // send
-        // return resident
+        // strip ObjCode from all extra things
+        auto contents = getContents(objCode);
+        // find its size
+        auto contentsSize = reinterpret_cast<AddrT>(contents.size());
+
+        // alloc memory of needed type
+        auto memRegion = memManager->alloc(contentsSize, memType);
+        if (memRegion.size >= contentsSize) {
+
+            // send contents through IConnection
+            conn->write(memRegion.start, contentsSize, contents.data());
+
+            // log for debug
+            utils::dumpSections(*objCode.getBinary());
+            utils::dumpSymbols(*objCode.getBinary());
+
+            // todo: load only '.text' section. otherwise need to implement relocations and all the stuff here
+            // get offset of needed symbol
+            if (auto sectionOffset = objCode.getSymbol().getAddress()) {
+                auto addr = sectionOffset.get();
+                // create unloadable resident
+                return ResidentObjCode<AddrT, RetT, Args...>(memManager, memRegion, addr, true);
+
+            } else {
+                // todo: handle some curious thing with unknown address (offset???) of the symbol
+            }
+        } else {
+            // todo: throw mem error
+        }
     };
 
     template<typename AddrT, typename RetT, typename... Args>
-    static bool checkEquality(conn::IConnection *c, mem::MemRegion<AddrT> memRegion,
+    static ResidentObjCode<AddrT, RetT, Args...> load(conn::IConnection<AddrT> *conn,
+                                               mem::MemRegion<AddrT> memRegion,
+                                               const ObjCode<RetT, Args...> &objCode) {
+        //
+
+        // call private_load
+    };
+
+    template<typename AddrT, typename RetT, typename... Args>
+    static bool checkEquality(conn::IConnection<AddrT> *conn,
+                              mem::MemRegion<AddrT> memRegion,
                               const ObjCode<RetT, Args...> &objCode);
+
+private:
+    template<typename RetT, typename... Args>
+    static content_t getContents(const ObjCode<RetT, Args...> &objCode) {
+        // strip ObjCode from all extra things and
+        // return actual contents
+        return objCode.getBinary()->getData();
+    };
+
 };
 
 
@@ -56,15 +103,16 @@ public:
 // example function: avoid all temp. objects (IRModule, ObjCode) if they're not needed
 // todo: this ex. shows that all funcs must have && counterparts \
 // todo: and all util objects must obey move semantics (not clear about copy)
-template<typename RetT, typename... Args>
-ResidentObjCode<RetT, Args...> convenientLoad(const DSLFunction<RetT, Args...> &target) {
+//template<typename RetT, typename... Args>
+//ResidentObjCode<RetT, Args...> convenientLoad(const DSLFunction<RetT, Args...> &target) {
+//
+//    IRModule<RetT, Args...> irModule = CodeGen::generateIR(target);
+//    if (bool success = CodeGen::link(irModule)) {
+//        return CodeLoader::load(CodeGen::compile(irModule));
+//    } else {
+//        return nullptr;
+//    }
+//};
 
-    IRModule<RetT, Args...> irModule = CodeGen::generateIR(target);
-    if (bool success = CodeGen::link(irModule)) {
-        return CodeLoader::load(CodeGen::compile(irModule));
-    } else {
-        return nullptr;
-    }
-};
 
 }
