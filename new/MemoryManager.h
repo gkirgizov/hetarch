64372a8c098bb5678ee18d5f93e2a8a1e58ca07b
gkirgizov <gkirgizov@gmail.com>
2018-01-09 22:49:40 +0300

    MemoryManager: basic implementation of SequentialFit

--- new/MemoryManager.h
+++ new/MemoryManager.h
@@ -4,7 +4,8 @@
 #include <vector>
 #include <memory>
 
-#include "boost/interprocess/mem_algo/simple_seq_fit.hpp"
+#include <unordered_map>
+#include <set>
 
 
 namespace hetarch {
@@ -20,6 +21,7 @@ enum class MemType {
 
 template<typename AddrT>
 struct MemRegion {
+    explicit MemRegion() = default;  // for constructing false-y value
     MemRegion(const AddrT start, const AddrT size, const MemType memType)
     : size(size), start(start), end(start+size), memType(memType)
     {}
@@ -28,18 +30,13 @@ struct MemRegion {
     const AddrT start;
     const AddrT end;
     const MemType memType;
-};
 
+    explicit operator bool() const { return size == 0; }
+};
 
 
 template<typename AddrT>
 class MemManager {
-
-    // for Boost.Interprocess
-    struct mutex_family {
-        typedef boost::interprocess::interprocess_mutex mutex_type;
-        typedef boost::interprocess::interprocess_recursive_mutex recursive_mutex_type;
-    };
 
     using void_ptr = void *;
     // As we allocate memory on the different machine,
@@ -47,47 +44,137 @@ class MemManager {
     // to address its (different machine's) memory.
     // So, this assert states exactly this.
     static_assert(sizeof(void_ptr) >= sizeof(AddrT));
-    using m_impl_t = boost::interprocess::simple_seq_fit<mutex_family, void_ptr>;
-    using size_type = typename m_impl_t::size_type;
-
-//    constexpr auto m_mcoef = sizeof(void_ptr) / sizeof(AddrT);
-
-    m_impl_t m_impl;
 
 public:
-    // MemManager will allocate memory from provided regions
-    explicit MemManager(const MemRegion<AddrT> &memAvailable)
-    : m_impl(static_cast<size_type>(memAvailable.size), static_cast<size_type>(AddrT()))
-    {}
-
-//    MemManager(const std::vector<MemRegion<AddrT>> &memAvailable);
-
-    MemRegion<AddrT> alloc(AddrT memSize, MemType memType);
+    virtual MemRegion<AddrT> alloc(AddrT memSize, MemType memType) = 0;
 
     // to control memory layout manually -- 'register' memory usage
-    // todo: check if it is okey for interface, encapsulation etc.
-    bool tryAlloc(const MemRegion<AddrT> &memRegion);
+    virtual bool tryAlloc(const MemRegion<AddrT> &memRegion) = 0;
 
-//     todo: should we be quite strict regarding invalid input? are only allocated reg-s allowed?
-    void free(const MemRegion<AddrT> &memRegion);
+    virtual void free(const MemRegion<AddrT> &memRegion) = 0;
+
+    /// Free without any checks. Can be easily misused. Only for the most confident callers who know definetly what they've allocated.
+    /// \param memRegion previously allocated region
+    /// \return true if memRegion has consistent MemType
+    virtual bool unsafeFree(const MemRegion<AddrT> &memRegion) {
+
+    virtual ~MemManager() = default;
 
 };
 
 
 
-template<typename AddrT>
-MemRegion<AddrT> MemManager<AddrT>::alloc(AddrT memSize, MemType memType) {
-    // won't compile with -fpermissive when sizeof(AddrT) < sizeof(void_ptr) because of loss of precision
-    if (auto offset = reinterpret_cast<AddrT>(m_impl.allocate(memSize))) {
+namespace mem_mgr_impl {
+    struct cmp_size_less {
+        template<typename AddrT>
+        bool operator()(const MemRegion<AddrT> &a, const MemRegion<AddrT> &b) {
+            // small to big: best fit
+            return a.size < b.size;
+        }
+    };
 
-        AddrT segmentStart = 0;
+    struct cmp_size_more {
+        template<typename AddrT>
+        bool operator()(const MemRegion<AddrT> &a, const MemRegion<AddrT> &b) {
+            // big to small: worst fit
+            return a.size > b.size;
+        }
+    };
 
-        AddrT start = offset + segmentStart;
-        return MemRegion<AddrT>(start, memSize, memType);
-    } else {
-        // todo: handle not enough memory (when offset == 0)
-    }
+    struct cmp_start_less {
+        template<typename AddrT>
+        bool operator()(const MemRegion<AddrT> &a, const MemRegion<AddrT> &b) {
+            // natural ordering
+            return a.start < b.start;
+        }
+    };
+
 }
+
+
+template<typename AddrT>
+class MemManagerSequentialFit: public MemManager<AddrT> {
+
+    using impl_cont_t = std::multiset<MemRegion, mem_mgr_impl::cmp_size_less>;
+
+    std::unordered_map<MemType, impl_cont_t> freeRegions{};
+
+    // todo: coalesce blocks when natural (by-address) ordering
+    void insertRegion(MemRegion<AddrT> newRegion, impl_cont_t &regions) {
+        regions.insert(newRegion);
+    }
+
+public:
+    explicit MemManagerSequentialFit(MemRegion<AddrT> availableMem) {
+        freeRegions[availableMem.memType].insert(std::move(availableMem));
+    }
+
+    explicit MemManagerSequentialFit(const std::vector<MemRegion<AddrT>> &availableMem) {
+        for (MemRegion<AddrT> memRegion : availableMem) {
+            freeRegions[memRegion.memType].insert(std::move(memRegion));
+        }
+    }
+
+    MemRegion<AddrT> alloc(AddrT memSize, MemType memType) override {
+        // todo: if there's no memory of such type, then fallback to less-restrictive memType
+        // don't forget to rewrite memType
+        auto regions = freeRegions[memType];
+
+        // todo: not the most efficient; for best-fit, worst-fit etc. can search better than linear
+        for (auto it = std::begin(regions); it != std::end(regions); ++it) {
+            if (memSize <= it->size) {
+                regions.erase(it);
+                MemRegion<AddrT> splittedRegion(it->start + memSize, it->size - memSize, memType);
+                insertRegion(splittedRegion, regions);
+                MemRegion<AddrT> foundRegion(it->start, memSize, memType);
+                return foundRegion;
+            }
+        }
+        return MemRegion<AddrT>();
+    }
+
+    bool tryAlloc(const MemRegion<AddrT> &memRegion) override {
+        auto memType = memRegion.memType;
+        auto regions = freeRegions[memType];
+
+        // todo: not the most efficient; for best-fit, worst-fit etc. can fail earlier
+        for (auto it = std::begin(regions); it != std::end(regions); ++it) {
+            if (memRegion.start >= it->start && memRegion.end <= it->end) {
+                regions.erase(it);
+                if (memRegion.start > it->start) {
+                    regions.insert(MemRegion<AddrT>(it->start, memRegion.start - it->start, memType));
+                }
+                if (memRegion.end < it->end) {
+                    regions.insert(MemRegion<AddrT>(memRegion.end, it->end - memRegion.end));
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void free(const MemRegion<AddrT> &memRegion) override {
+        // find ALL overlapped blocks
+        // among them find 2 'border' blocks
+        // find overlaps with them
+        // split them correspondingly
+        // erase ALL except last two overlapped blocks from freeregions
+        // erase these 2 smaller overlaps
+        // insert memRegion to freeRegions
+    }
+
+    bool unsafeFree(const MemRegion<AddrT> &memRegion) override {
+        try {
+            auto regions = freeRegions.at(memRegion.memType);
+            regions.insert(memRegion);
+            return true;
+        } catch(const std::out_of_range &e) {
+            return false;
+        }
+    }
+
+};
+
 
 }
 }
