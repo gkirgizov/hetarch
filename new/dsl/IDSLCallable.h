8bae8175d91f0312228af58463706bbdbc975605
gkirgizov <gkirgizov@gmail.com>
2018-01-15 00:35:23 +0300

    DSL: work on Var, add Return, add FuncParam sketch

--- new/dsl/IDSLCallable.h
+++ new/dsl/IDSLCallable.h
@@ -3,8 +3,11 @@
 
 #include <utility>
 #include <type_traits>
+#include <string_view>
 
 
+
+// todo: remove
 template<typename T>
 class IDSLVariable {
 public:
@@ -29,17 +32,27 @@ class EBinOp;
 class IRGen {
 public:
 //    template<typename T, typename std::enable_if<std::is_base_of<DSLBase, T>::value>::type>
-    template<typename T>
-    void accept(const T &thing) const {}
 
-    template<>  // full specialisation of func template -- this is how actual work will be implemented
-    void accept(const DSLFunction &func) const {}
+    // general template
+//    template<typename T>
+//    void accept(const T &thing) const {}
+//
+    // full specialisation of func template -- this is how actual work will be implemented
+//    template<>
+//    void accept(const ThingType &thing) const {}
+
+    // todo: check is it compiles given other overloads of accept
+    template<typename RetT, typename ...Args>  // full specialisation of func template -- this is how actual work will be implemented
+    void accept(const DSLFunction<RetT, Args...> &func) const {}
+
+    // maybe i doesn't even need this if i'll put handling of Return<RetT> in DSLFunction<RetT, Args...>
+    template<typename RetT>
+    void accept(const Return<RetT> &returnSt) const {}
 };
 
 
 
 struct DSLBase {
-    // void is temporary. maybe it is so, but also maybe there's some llvm::IRBase object
     virtual void toIR(const IRGen &gen) const = 0;
 };
 
@@ -58,7 +71,7 @@ struct ExprBase : public DSLBase {
 
 template<typename T>
 struct Expr : public ExprBase {
-
+//    using type = T;
 };
 
 
@@ -107,6 +120,7 @@ constexpr EBinOp<T> operator+(Expr<T> &&lhs, Expr<T> &&rhs) {
 
 
 
+// todo: is it Expr?
 template<typename T>
 struct Assignable: public Expr<T> {
     constexpr EAssign<T> operator=(Expr<T> &&rhs) {
@@ -117,35 +131,72 @@ struct Assignable: public Expr<T> {
 
 // todo: enable_if only for integral etc.?
 // todo: maybe move is_volatile to c-tor?
+//  --wouldn't be very good. conceptually volatile is a part of type
+//  todo: same about Const. maybe move there?
 template<typename T, bool is_volatile = false>
-class Var {
+struct Var : public DSLBase {
+    explicit constexpr Var() = default;
+    explicit constexpr Var(const std::string_view &name) : initial_val{}, name{name} {}
+    explicit constexpr Var(T &&value, const std::string_view &name = "")
+            : initial_val{std::forward(value)}, name{name}
+    {}
+
+    constexpr const std::string_view name;
 protected:
-    T val;
-public:
-    explicit constexpr Var() : val{} {}
-    explicit constexpr Var(T &&value) : val{std::forward(value)} {}  // actually non-explicit is okey
+    constexpr T initial_val;
+};
 
-//    explicit constexpr Var(const IDSLVariable<T> &value) : val{value.read()} {}
-//    explicit constexpr Var(IDSLVariable<T> &&value) : val{value.read()} {}
+// todo: handle virtual inheritance (somewhere...) higher in the hierarchy
+template<typename T>
+struct LocalVar : public Var<T>, public Assignable<T> {
+    using Var<T>::Var<T>;
+};
+
+// todo: the difference with LocalVar is that it is Loadable
+template<typename T>
+struct GlobalVar : public Var<T>, public Assignable<T> {
+    using Var<T>::Var<T>;
 };
 
 template<typename T>
-class LocalVar : public Var<T>, public Assignable<T> {
-};
+struct ConstVar : public Var<T> {
+    using Var<T>::Var<T>;
 
-// the difference with LocalVar is that it is Loadable
-template<typename T>
-class GlobalVar : public Var<T>, public Assignable<T> {
-};
-
-template<typename T>
-class ConstVar : public Var<T> {
-public:
     constexpr void substitute(T &&value) {
-        val = std::forward(value);
+        initial_val = std::forward(value);
     }
 };
 
+
+//template<typename T, typename std::enable_if<std::is_base_of<Var, T>::value>::type>
+//class FuncParam : public DSLBase
+
+// this elaborate templated inheritance is to mimic the behaviour of Var<T>
+// because FuncParam should behave exactly in the same way as usual Var does
+// todo: doesn't make sense for VarT == GlobalVar
+template<template<typename T> typename VarT,
+        typename std::enable_if<std::is_base_of<Var<T>, VarT<T>>::value>::type>
+struct FuncParam : public VarT<T> {
+    using VarT<T>::VarT<T>;
+
+    explicit constexpr FuncParam(T &&default_value, const std::string_view &name = "")
+            : VarT<T>{default_value, name}, defaulted{true}
+    {}
+
+    constexpr bool defaulted{false};
+};
+
+
+// works well when we can refer to enclosed construct independently of its nature
+// i.e. toIR override { return "return " + x.getIRReferenceName(); }
+template<typename RetT>
+class Return : public DSLBase {
+    constexpr const DSLBase &returnee;
+public:
+    explicit constexpr Return(const Var<RetT> &var) : returnee{var} {};
+    explicit constexpr Return(const Expr<RetT> &expr) : returnee{expr} {};
+
+};
 
 
 template<typename RetT, typename... Args>
@@ -163,7 +214,8 @@ public:
 //    template<typename ...Ts>
 //    constexpr explicit DSLFunction(Expr<Ts>&&... statements, Expr<RetT> &&returnSt); // imitates Seq, may even use it
 
-    constexpr DSLFunction(const FuncParams<Args...> &args, ExprBase&&... statements, Expr<RetT> &&returnSt); // imitates Seq, may even use it
+    template<typename T>
+    constexpr DSLFunction(const FuncParams<Args...> &args, Seq<T> statements, Return<RetT> &&returnSt);
     constexpr DSLFunction(const FuncParams<Args...> &args, Seq<RetT> statements);
 
 //    constexpr IDSLVariable<RetT> call(IDSLVariable<Args>... args) override;
@@ -182,7 +234,10 @@ struct FuncParams {
 
 template<typename RetT, typename ...Args>
 static void generateIR(const DSLFunction<RetT, Args...> &f) {
-
+    int n, test;
+    n, test;
+    n;
+    test;
 };
 
 
