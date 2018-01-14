7ba455318d4b5122230551e40faa05151487dd4e
gkirgizov <gkirgizov@gmail.com>
2018-01-14 15:29:16 +0300

    minor: remove a lot of old commented code

--- new/supportingClasses.h
+++ new/supportingClasses.h
@@ -11,8 +11,8 @@ class ISignature {
 
 public:
     explicit ISignature() = default;
-    explicit ISignature(const std::string &symbolName)
-            : symbol(symbolName) {}
+    explicit ISignature(std::string symbolName)
+            : symbol{std::move(symbolName)} {}
 
     const std::string symbol;
 
@@ -23,7 +23,6 @@ public:
 template<typename RetT, typename... Args>
 class ObjCode : public ISignature<RetT, Args...> {
 
-    // temporary
     using m_payload_t = llvm::object::OwningBinary<llvm::object::ObjectFile>;
 
     m_payload_t m_payload;
@@ -88,63 +87,6 @@ public:
     {};
 
 };
-
-
-
-
-//class ObjCode2;
-//class CodeGen2;
-//
-//class IRModule2 {
-//
-//    struct IRModuleTypedBase {
-//        virtual ~IRModuleTypedBase() {};
-//
-////         there goes common (to what?) interface?
-////        virtual ?type? getSignature();
-//
-//        virtual ObjCode2 compile(CodeGen2 *) const = 0;
-//    };
-//
-//    template<typename RetT, typename... Args>
-//    struct IRModuleTyped : IRModuleTypedBase {
-//
-////        const ISignature<RetT, Args...> m_signature;
-//
-////        explicit IRModuleTyped(const ISignature<RetT, Args...> &signature)
-////        : m_signature(signature)
-////        {}
-//
-//        llvm::Module m;
-//
-//        explicit IRModuleTyped(llvm::Module &m)
-//        : m(std::move(m))
-//        {};
-//
-//        ObjCode2 compile(CodeGen2 *codeGen) const override {
-//            return codeGen.compile()
-//        }
-//    };
-//
-//
-//    friend class CodeGen; // no need to expose getters for this->m because they're needed only for CodeGen
-//
-//    IRModuleTypedBase m_signatureHolder;
-//
-//    llvm::Module m;
-//    std::vector <IRModule2> m_dependencies;
-//
-//public:
-//
-//    template<typename RetT, typename... Args>
-//    explicit IRModule2(llvm::Module &m, const ISignature<RetT, Args...> &signature)
-//            : m(std::move(m)), m_dependencies(), m_signatureHolder(IRModuleTyped(signature))
-//    {};
-//
-////    auto getSignature() const {
-////        return m_signatureHolder.
-////    }
-//};
 
 
 }
