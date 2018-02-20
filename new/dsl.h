5d79576d3d4751d6538d43b9b654bc2142e0e656
gkirgizov <gkirgizov@gmail.com>
2018-02-20 10:15:20 +0300

    minor: change project linker; add one dsl.h header for import

---
+++ new/dsl.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "dsl/dsl_base.h"
+#include "dsl/var.h"
+//#include "dsl/array.h"
+//#include "dsl/ptr.h"
+#include "dsl/fun.h"
+#include "dsl/ResidentObjCode.h"
+#include "dsl/sequence.h"
+#include "dsl/if_else.h"
+#include "dsl/loops.h"
+#include "dsl/op.h"
